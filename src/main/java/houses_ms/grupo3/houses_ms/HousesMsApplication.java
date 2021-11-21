0d533cebbebcd07c2ad03cff34d9fce209914658
Javier Tellez <jato.25@hotmail.com>
2021-11-20 22:17:35 -0500

    first commit

---
+++ src/main/java/houses_ms/grupo3/houses_ms/HousesMsApplication.java
@@ -0,0 +1,13 @@
+package houses_ms.grupo3.houses_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousesMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/houses_ms/grupo3/houses_ms/HousesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package houses_ms.grupo3.houses_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
